803e4ff95faabd62fb9d1b6b4edb1e6a624b771c
Kati <tetereva_em@mai.ru>
2021-07-20 16:58:35 +0900

    location reload after game end

--- js/script.js
+++ js/script.js
@@ -15,25 +15,27 @@ var arrWin =  [
 gameArea.addEventListener('click', e => {
     if(e.target.innerHTML != '×' && e.target.innerHTML != '○'){
         if(e.target.className = 'cell'){
-         turn % 2 === 0 ? e.target.innerHTML  = '×' : e.target.innerHTML = '○';
-         turn++;
-            isWinner();
+            turn % 2 === 0 ? e.target.innerHTML  = '×' : e.target.innerHTML = '○';
+            turn++;
         }
+        isWinner();
     }
 })
 
 function isWinner(){
     var cells = document.getElementsByClassName('cell');
-    for(let i = 0; i < arrWin.length; i++){
-        if (cells[arrWin[i][0]].innerHTML == '×' && cells[arrWin[i][1]].innerHTML =='×' && cells[arrWin[i][2]].innerHTML =='×'){
-            result = '×';
-            alert(result + " are win!");
+    if(result !='×' && result != '○'){
+        for(let i = 0; i < arrWin.length; i++){
+            if (cells[arrWin[i][0]].innerHTML == '×' && cells[arrWin[i][1]].innerHTML =='×' && cells[arrWin[i][2]].innerHTML =='×'){
+                result = '×';
+                alert(result + " are win!");
             
-        } else if (cells[arrWin[i][0]].innerHTML == '○' && cells[arrWin[i][1]].innerHTML =='○' && cells[arrWin[i][2]].innerHTML =='○'){
-            result = '○';
-            alert(result + " are win!");
+            } else if (cells[arrWin[i][0]].innerHTML == '○' && cells[arrWin[i][1]].innerHTML =='○' && cells[arrWin[i][2]].innerHTML =='○'){
+                result = '○';
+                alert(result + " are win!");
+            }
         }
+    } else {
+        location.reload();
     }
-    
-    
 }
